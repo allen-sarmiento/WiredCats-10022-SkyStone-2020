8a5d3c663792aca4372bb8233917690c5b875fe9
cyberLeelad <55859431+cyberLeelad@users.noreply.github.com>
2019-12-07 09:45:26 -0500

    Update SkyStone10022Experimental.java
    changed intake settings. changed gamepad settings. maybe other stuff? not 100% sure

--- SkyStone10022Experimental.java
+++ SkyStone10022Experimental.java
@@ -7,26 +7,29 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 /**
  *      CONTROL SCHEME:
  *
- *      A: Automatic Win
+ *      Gamepad 1:
+
+ *      Right Bumper: Intake toggle
+ *      Left Bumper: Outtake toggle
+ *
+ *      Left Joystick Y-Axis: Forward/Backward
+ *      Left Joystick X-Axis: Strafing
+ *      Right Joystick X-Axis: Rotation
+ *
+ *      Right Trigger: Half Robot Speed
+ *
+ *      Gamepad 2:
+ *
  *      B: Set Foundation Hook
  *      X: Open/Close Claw
  *      Y: Rotate Claw
  *
  *      Up: Raise Vertical Slides
- *      Down: Lower Vertical SLides
+ *      Down: Lower Vertical Slides
  *      Left: Retract Horizontal Slides
  *      Right: Extend Horizontal Slides
  *
- *      Right Bumper: Intake Block *gamepad 1
- *      Left Bumper: Outtake Block *gamepad 1
- *
- *      *gamepad1
- *      Left Joystick Y-Axis: Forward/Backward
- *      Left Joystick X-Axis: Strafing
- *      Right Joystick X-Axis: Rotation
- *
- *      Left Joystick Button: Half Robot Speed *gamepad1
- *      Right Joystick Button: Half Robot Speed *gamepad1 */
+ */
 
 public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
 
@@ -45,55 +48,62 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
 
         while (opModeIsActive()) {
 
-            // START ROBOT-CENTRIC DRIVETRAIN
+            /* ROBOT-CENTRIC DRIVETRAIN */ {
 
-            float lefty = -gamepad1.left_stick_y;
-            float leftx = gamepad1.left_stick_x;
-            float rightx = gamepad1.right_stick_x;
+                float lefty = -gamepad1.left_stick_y;
+                float leftx = gamepad1.left_stick_x;
+                float rightx = gamepad1.right_stick_x;
 
-            // Joystick deadzones prevents unintentional drivetrain movements
+                // Joystick deadzones prevents unintentional drivetrain movements
 
-            if (Math.abs(lefty) <= 0.2) {
+                if (Math.abs(lefty) <= 0.2) {
 
-                lefty = 0;
+                    lefty = 0;
+                }
+                if (Math.abs(leftx) <= 0.2) {
+
+                    leftx = 0;
+                }
+                if (Math.abs(rightx) <= 0.2) {
+
+                    rightx = 0;
+                }
+
+                // Motor powers are set to the power of 3 so that the drivetrain motors accelerates
+                // exponentially instead of linearly
+
+                flpower = Math.pow((lefty + leftx + rightx), 3);
+                blpower = Math.pow((lefty - leftx + rightx), 3);
+                frpower = Math.pow((lefty - leftx - rightx), 3);
+                brpower = Math.pow((lefty + leftx - rightx), 3);
+
+                // Motor Power is halved while either joystick button is held down to allow for
+                // more precise robot control
+
+                if (gamepad1.right_trigger > 0.9) {
+
+                    flpower /= 2;
+                    frpower /= 2;
+                    blpower /= 2;
+                    brpower /= 2;
+                }
+                else if (gamepad1.right_trigger > 0.1) {
+
+                    rTrigger = (int)((1 - gamepad1.right_trigger ) /2 + 0.5);
+
+                    flpower *= rTrigger;
+                    frpower *= rTrigger;
+                    blpower *= rTrigger;
+                    brpower *= rTrigger;
+                }
+
+                frontLeft.setPower(flpower);
+                backLeft.setPower(blpower);
+                frontRight.setPower(frpower);
+                backRight.setPower(brpower);
             }
-            if (Math.abs(leftx) <= 0.2) {
 
-                leftx = 0;
-            }
-            if (Math.abs(rightx) <= 0.2) {
-
-                rightx = 0;
-            }
-
-            // Motor powers are set to the power of 3 so that the drivetrain motors accelerates
-            // exponentially instead of linearly
-
-            flpower = Math.pow((lefty + leftx + rightx), 3);
-            blpower = Math.pow((lefty - leftx + rightx), 3);
-            frpower = Math.pow((lefty - leftx - rightx), 3);
-            brpower = Math.pow((lefty + leftx - rightx), 3);
-
-            // Motor Power is halved while either joystick button is held down to allow for
-            // more precise robot control
-
-            if (gamepad1.left_stick_button || gamepad1.right_stick_button) {
-
-                flpower /= 2;
-                frpower /= 2;
-                blpower /= 2;
-                brpower /= 2;
-            }
-
-            frontLeft.setPower(flpower);
-            backLeft.setPower(blpower);
-            frontRight.setPower(frpower);
-            backRight.setPower(brpower);
-
-            // END ROBOT-CENTRIC DRIVETRAIN
-
-            // START FOUNDATION HOOK
-            {
+            /* foundation hook */ {
                 if (gamepad2.b && bToggle == 0) {
 
                     bToggle = 1;
@@ -113,10 +123,8 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
                     bToggle = 0;
                 }
             }
-            // END FOUNDATION HOOK
 
-            // START CLAW ACTIVATE
-            {
+            /* CLAW ACTIVATE */ {
                 if (gamepad2.x && xToggle == 0) {
 
                     xToggle = 1;
@@ -124,6 +132,7 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
                 } else if (!gamepad2.x && xToggle == 1) {
 
                     activateClaw();
+                    intakeOff();
                     xToggle = 2;
 
                 } else if (gamepad2.x && xToggle == 2) {
@@ -136,10 +145,8 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
                     xToggle = 0;
                 }
             }
-            // END CLAW ACTIVATE
 
-            // START CLAW ROTATE
-            {
+            /* CLAW ROTATE */ {
                 if (gamepad2.y && yToggle == 0) {
 
                     yToggle = 1;
@@ -169,10 +176,8 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
                 }
 
             }
-            // END CLAW ROTATE
 
-            // START VERTICAL SLIDES
-            {
+            /* vertical slides */ {
                 if (gamepad2.dpad_up) {
 
                     ySlidesUp();
@@ -187,10 +192,8 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
 
                 }
             }
-            // END VERTICAL SLIDES
 
-            // START HORIZONTAL SLIDES
-            {
+            /* horizontal slides */ {
                 if (gamepad2.dpad_right) {
 
                     xSlideForward();
@@ -204,12 +207,27 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
                     xSlideOff();
                 }
             }
-            // END HORIZONTAL SLIDES
+
+            /* intake HOLD-- inactive */
+            /** intakes by default
+             *
+             *  while left bumper is pressed = outtake
+             *  while right bumper is pressed = intake off
+             *  else intakes
 
 
-            // START INTAKE
-            // intake
-            {
+             if (gamepad1.left_bumper) {
+             outtake();
+             }
+             else if (gamepad1.right_bumper) {
+             intakeOff();
+             }
+             else {
+             intake();
+             }
+             */
+
+            /* intake */ {
                 if (gamepad1.right_bumper && rBumperToggle == 0) {
 
                     rBumperToggle = 1;
@@ -225,32 +243,31 @@ public class SkyStone10022Experimental extends SkyStone10022LinearOpMode {
 
                 } else if (!gamepad1.right_bumper && rBumperToggle == 3) {
 
-                    intakeOff();
+                    //outtake
+                    {
+                        if (gamepad1.left_bumper && lBumperToggle == 0) {
+
+                            lBumperToggle = 1;
+
+                        } else if (!gamepad1.left_bumper && lBumperToggle == 1) {
+
+                            outtake();
+                            lBumperToggle = 2;
+
+                        } else if (gamepad1.left_bumper && lBumperToggle == 2) {
+
+                            rBumperToggle = 3;
+
+                        } else if (!gamepad1.right_bumper && lBumperToggle == 3) {
+
+                            intakeOff();
+                            lBumperToggle = 0;
+                        }
+                    }
+
                     rBumperToggle = 0;
                 }
             }
-            //outtake
-            {
-                if (gamepad1.left_bumper && lBumperToggle == 0) {
-
-                    lBumperToggle = 1;
-
-                } else if (!gamepad1.left_bumper && lBumperToggle == 1) {
-
-                    outtake();
-                    lBumperToggle = 2;
-
-                } else if (gamepad1.left_bumper && lBumperToggle == 2) {
-
-                    rBumperToggle = 3;
-
-                } else if (!gamepad1.right_bumper && lBumperToggle == 3) {
-
-                    intakeOff();
-                    lBumperToggle = 0;
-                }
-            }
-            // END INTAKE
 
         }
     }
