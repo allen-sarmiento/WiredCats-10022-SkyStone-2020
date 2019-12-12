85da41a85472c240b474b5e41c7c92c88aa459ca
Allen Sarmiento <43957614+matchaicedtea@users.noreply.github.com>
2019-12-12 14:28:12 -0500

    Update SkyStone10022Autonomous.java

--- SkyStone10022Autonomous.java
+++ SkyStone10022Autonomous.java
@@ -12,24 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
 
 @Autonomous
 
 public class SkyStone10022Autonomous extends SkyStone10022LinearOpMode {
 
-    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;//use back camra
-    private static final boolean PHONE_IS_PORTRAIT = false ;
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;//use back camera
     private static final String VUFORIA_KEY = "AUBZ9tz/////AAABmcD+zzeDb02lt3WkDlYB/vZi/6YK4SHRNFLfBrST0dK16ImQjN+KG3vN2MFs/EAcJydoBmOH0nbqY9lxbtNPKSpxOMtnrDwQmZWds+Z74kjqSmMCKDGT1a3LIIOF+6jbEWgITgiBRlY+gGD/b8m6Ck3jCoe+CyVhXv1zyOKtcjlWLBHGhBSQ/xJbHGdkDsah2WkFJGUaaXRWLHqnYyit/FKJzbQ5UjyFUraZZoTTXjgjfRvM7/YcwwDf+CXYCYObPKANY0g/y9YaArDYS2bgDL/5Fh9E3SUhAv8CpNprIA2T8GCZhMDzFJYme87N1+1DspG7+2AsEyabBSKhst11vV6Z8tWRcHCfspKeEO/LtO/B";
 
     // Class Members
     private VuforiaLocalizer vuforia = null;
 
     String sequence = "";
+    int position = 0;
+    double deliveryDistance;
+    double returnDistance;
 
-    boolean isSkyStone;
-    int skyStonePos;
-
-    @Override public void runOpMode() {
+    @Override
+    public void runOpMode() {
 
         initialize();
 
@@ -37,7 +38,7 @@ public class SkyStone10022Autonomous extends SkyStone10022LinearOpMode {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection   = CAMERA_CHOICE;
+        parameters.cameraDirection = CAMERA_CHOICE;
 
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
@@ -45,30 +46,6 @@ public class SkyStone10022Autonomous extends SkyStone10022LinearOpMode {
 
         VuforiaTrackable stoneTarget = targetsSkyStone.get(0);
         stoneTarget.setName("Stone Target");
-        VuforiaTrackable blueRearBridge = targetsSkyStone.get(1);
-        blueRearBridge.setName("Blue Rear Bridge");
-        VuforiaTrackable redRearBridge = targetsSkyStone.get(2);
-        redRearBridge.setName("Red Rear Bridge");
-        VuforiaTrackable redFrontBridge = targetsSkyStone.get(3);
-        redFrontBridge.setName("Red Front Bridge");
-        VuforiaTrackable blueFrontBridge = targetsSkyStone.get(4);
-        blueFrontBridge.setName("Blue Front Bridge");
-        VuforiaTrackable red1 = targetsSkyStone.get(5);
-        red1.setName("Red Perimeter 1");
-        VuforiaTrackable red2 = targetsSkyStone.get(6);
-        red2.setName("Red Perimeter 2");
-        VuforiaTrackable front1 = targetsSkyStone.get(7);
-        front1.setName("Front Perimeter 1");
-        VuforiaTrackable front2 = targetsSkyStone.get(8);
-        front2.setName("Front Perimeter 2");
-        VuforiaTrackable blue1 = targetsSkyStone.get(9);
-        blue1.setName("Blue Perimeter 1");
-        VuforiaTrackable blue2 = targetsSkyStone.get(10);
-        blue2.setName("Blue Perimeter 2");
-        VuforiaTrackable rear1 = targetsSkyStone.get(11);
-        rear1.setName("Rear Perimeter 1");
-        VuforiaTrackable rear2 = targetsSkyStone.get(12);
-        rear2.setName("Rear Perimeter 2");
 
         List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(targetsSkyStone);
@@ -77,190 +54,112 @@ public class SkyStone10022Autonomous extends SkyStone10022LinearOpMode {
 
         waitForStart();
 
-        forward(0,0);
+        // strafeLeft(0,0); // To foundation
 
-        // Scans all trackables
-        for (VuforiaTrackable trackable : allTrackables) {
+        sleep(300);
 
-            // If any trackable is visible
-            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+        // setHookDown(); // Grab foundation
 
-                if (trackable.getName().equals("Blue Perimeter 1")) {
+        sleep(300);
 
-                    telemetry.addLine("Blue Perimeter 1 is visible");
-                    sequence = "Blue Perimeter 1";
-                }
+        // strafeRight(0,0); // Drag foundation to depot
 
-                if (trackable.getName().equals("Blue Perimeter 2")) {
+        sleep(300);
 
-                    telemetry.addLine("Blue Perimeter 2 is visible");
-                    sequence = "Blue Perimeter 2";
-                }
+        // setHookUp();
 
-                if (trackable.getName().equals("Red Perimeter 1")) {
+        sleep(300);
 
-                    telemetry.addLine("Red Perimeter 1 is visible");
-                    sequence = "Red Perimeter 1";
-                }
+        // forward(0,0); // Align to first stone
 
-                if (trackable.getName().equals("Red Perimeter 2")) {
+        sleep(300);
 
-                    telemetry.addLine("Red Perimeter 2 is visible");
-                    sequence = "Red Perimeter 2";
-                }
+        // rotateRight(0,0); // Face camera to quarry
 
-                if (trackable.getName().equals("Stone Target")) {
+        sleep(300);
 
-                    telemetry.addLine("SkyStone is visible");
-                    isSkyStone = true;
-                }
+        // forward(0,0); // Move to first stone
 
-                else {
+        sleep(300);
 
-                    isSkyStone = false;
-                }
-            }
-        }
+        // Find skystone position
+        while (position == 0) {
 
-        if (sequence.equals("Blue Perimeter 1")){
+            if (((VuforiaTrackableDefaultListener)stoneTarget.getListener()).isVisible()) {
 
-            strafeLeft(0, 0); // move to left-most quarry stone
+                position = 1;
 
-            rotateRight(0, 180); // face camera to quarry
-
-            sleep(250);
-
-            findSkyStonePositionBlue();
-
-            deliverStones();
-        }
-
-        if (sequence.equals("Blue Perimeter 2")){
-
-            forward(0,0); // move in front of build plate
-
-            rotateRight(0,90); // face claw to build plate
-
-            backward(0,0); // center robot to build plate
-
-            setHookDown();
-
-            strafeRight(0,0); // drag foundation to build zone
-
-            setHookUp();
-
-            forward(0,0);
-        }
-
-        if (sequence.equals("Red Perimeter 1")) {
-
-
-        }
-
-        if (sequence.equals("Red Perimeter 2")) {
-
-            forward(0,0); // move in front of build plate
-
-            rotateLeft(0,90); // face claw to build plate
-
-            forward(0,0); // center robot to build plate
-
-            setHookDown();
-
-            strafeRight(0,0); // drag foundation to build zone
-
-            setHookUp();
-
-            backward(0,0);
-        }
-
-        telemetry.update();
-    }
-
-    public void findSkyStonePositionBlue() {
-
-        if(isSkyStone) {
-
-            skyStonePos = 1;
-        }
-
-        else {
-
-            strafeLeft(0,0);
-
-            sleep(1000);
-
-            if (isSkyStone) {
-
-                skyStonePos = 2;
+                telemetry.addLine("Position 1");
+                telemetry.update();
             }
 
             else {
 
-                skyStonePos = 3;
+                // strafeLeft(0,0); // Move to next stone
+
+                sleep(300);
+
+                if (((VuforiaTrackableDefaultListener)stoneTarget.getListener()).isVisible()) {
+
+                    position = 2;
+
+                    telemetry.addLine("Position 2");
+                    telemetry.update();
+                }
+
+                else {
+
+                    position = 3;
+
+                    telemetry.addLine("Position 3");
+                    telemetry.update();
+                }
             }
         }
-    }
 
-    public void deliverStones() {
+        if (position == 1) {
 
-        if (skyStonePos == 1) {
-
-            rotateLeft(0, 90);
-
-            backward(0,0);
-
-            strafeLeft(0, 0);
-
-            intake();
-
-            forward(0,0);
-
-            strafeLeft(0,0);
-
-            backward(0,0);
-
-            rotateLeft(0, 90);
-
-            outtake();
-
-            sleep(750);
-
-            intakeOff();
-
-            rotateRight(0,90);
-
-            forward(0,90);
-
-            strafeLeft(0,0);
-
-            intake();
-
-            forward(0,0);
-
-            strafeRight(0,0);
-
-            backward(0,0);
-
-            rotateLeft(0,90);
-
-            outtake();
-
-            sleep(750);
-
-            intakeOff();
-
-            strafeRight(0,0);
+            deliveryDistance = 0;
+            returnDistance = 0;
         }
 
-        else if (skyStonePos == 2) {
+        else if (position == 2) {
 
-
+            deliveryDistance = 0;
+            returnDistance = 0;
         }
 
-        else{
+        else {
 
+            deliveryDistance = 0;
+            returnDistance = 0;
+        }
 
+        if (position == 1 || position == 2) {
+
+            // rotateRight(0,0); // Face intake to skystone
+
+            sleep(300);
+
+            // intake();
+
+            // forward(0,0); // Intake first skystone
+
+            sleep(300);
+
+            // backward(0,0);
+
+            sleep(300);
+
+            // rotateLeft(0,0);
+
+            sleep(300);
+
+            // forward(0, deliveryDistance);
+
+            sleep(300);
+
+            outtake();
         }
     }
 }
